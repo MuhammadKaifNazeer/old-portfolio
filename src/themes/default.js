515dfa22990e456094e4c6032e7018d1c3d38801
KaifNazeer <muhammadkaifnazeer99@gmail.com>
2023-09-25 16:04:11 +0500

    try to fix bugs

--- src/themes/default.js
+++ src/themes/default.js
@@ -18,7 +18,7 @@ export default {
     },
     // Breakpoints for responsive design
     breakpoints: {
-      sm: 'screen and (max-width: 640px)',
+      sm: 'screen and (max-width: 400px)',
       md: 'screen and (max-width: 768px)',
       lg: 'screen and (max-width: 1024px)',
       xl: 'screen and (max-width: 1280px)'
